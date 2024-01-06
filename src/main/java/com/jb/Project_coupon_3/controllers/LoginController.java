3023b9bc82291a20cae36f6e9d3e07dc2ff530df
Elia Slomka <elia@auto3p.com>
2024-01-06 20:08:06 +0100

    some refactoring

--- src/main/java/com/jb/Project_coupon_3/controllers/LoginController.java
+++ src/main/java/com/jb/Project_coupon_3/controllers/LoginController.java
@@ -1,12 +1,7 @@
 package com.jb.Project_coupon_3.controllers;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.jb.Project_coupon_3.exceptions.CouponSystemException;
-import com.jb.Project_coupon_3.models.Company;
-import com.jb.Project_coupon_3.models.Customer;
 import com.jb.Project_coupon_3.services.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,65 +9,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class LoginController {
 
     private LoginManager loginManager;
-    private Set<String> tokensStore = new HashSet<>();
-    //Dependency injection INSTEAD OF @Autowired on top of the Objects
+    // Dependency injection INSTEAD OF @Autowired on top of the Objects
 
-
-    public LoginController(LoginManager loginManager, Set<String> tokensStore) {
+    public LoginController(LoginManager loginManager) {
         this.loginManager = loginManager;
-        this.tokensStore = tokensStore;
     }
 
     @PostMapping("login")
-    public ResponseEntity<String> login(String email, String password, ClientType clientType ) throws CouponSystemException {
-            //login successful
-
-            ClientService service = loginManager.login(email, password, clientType);
-            String token = createToken(service, email, password);
-            //save token in store...
-            tokensStore.add(token);//TODO remove SERVICE, unnecessary if I'M saving the token
-            return ResponseEntity.status(HttpStatus.CREATED).body(token);
-    }
-
-    private String createToken (ClientService service, String email, String password){
-        String token = " ";
-        if (service instanceof CompanyService) {
-            Company company = ((CompanyService) service).getCompanyDetails(email, password);
-            Instant expires = Instant.now().plus(30, ChronoUnit.MINUTES);
-            token = JWT.create()
-                    .withClaim("id", company.getId())
-                    .withClaim("name", company.getName())
-                    .withClaim("role", 333)
-                    .withExpiresAt(expires)
-                    .sign(Algorithm.none());
-        }else if(service instanceof AdminService){
-            Instant expires = Instant.now().plus(30, ChronoUnit.MINUTES);
-            token = JWT.create()
-                    .withClaim("name", "admin")
-                    .withClaim("role",999 )
-                    .withExpiresAt(expires)
-                    .sign(Algorithm.none());
-      } else {
-            Customer customer = ((CustomerService) service).getCustomerDetails(email, password);
-            Instant expires = Instant.now().plus(30, ChronoUnit.MINUTES);
-            token = JWT.create()
-                    .withClaim("id", customer.getId())
-                    .withClaim("name", customer.getFirstName())
-                    .withClaim("lastName", customer.getLastName())
-                    .withClaim("role", 666)
-                    .withExpiresAt(expires)
-                    .sign(Algorithm.none());
-        }
-      return token;
+    public ResponseEntity<String> login(String email, String password, ClientType clientType)
+            throws CouponSystemException {
+        // login successful
+        String token = loginManager.login(email, password, clientType);
+        return ResponseEntity.status(HttpStatus.CREATED).body(token);
     }
 }
--- src/main/java/com/jb/Project_coupon_3/services/TokenService.java
+++ src/main/java/com/jb/Project_coupon_3/services/TokenService.java
@@ -2,7 +2,6 @@ package com.jb.Project_coupon_3.services;
 
 import com.auth0.jwt.JWT;
 import com.jb.Project_coupon_3.exceptions.CouponSystemException;
-import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,28 +14,26 @@ public class TokenService {
     @Autowired
     private Set<String> tokensStore;
 
-    @Autowired
-    private HttpServletRequest request;
-
-    public Integer getId(HttpServletRequest request , int  role) throws CouponSystemException {
-       String token = request.getHeader("Authorization").replace("Bearer ","");
-       int tokenRole =JWT.decode(token).getClaim("role").asInt();
-        if(!tokensStore.contains (token)){
+    public Integer getId(HttpServletRequest request, int role) throws CouponSystemException {
+        String token = request.getHeader("Authorization").replace("Bearer ", "");
+        int tokenRole = JWT.decode(token).getClaim("role").asInt();
+        if (!tokensStore.contains(token)) {
             throw new CouponSystemException("token does not exist in store", HttpStatus.UNAUTHORIZED);
         }
-        if (tokenRole == 999){
+        if (tokenRole == 999) {
             return -99;
         }
-        if(tokenRole != role){
+        if (tokenRole != role) {
             throw new CouponSystemException("role mismatch", HttpStatus.UNAUTHORIZED);
         }
         System.out.println(tokenRole);
         return Integer.parseInt(JWT.decode(token).getClaim("id").toString());
     }
 
-//    public String getRole(HttpServletRequest request) throws CouponSystemException {
-//        String token = request.getHeader("Authorization").replace("Bearer ", "");
-//
-//        return ;
-//    }
+    // public String getRole(HttpServletRequest request) throws
+    // CouponSystemException {
+    // String token = request.getHeader("Authorization").replace("Bearer ", "");
+    //
+    // return ;
+    // }
 }
